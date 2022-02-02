8c8acdd876956f5f2bee11768636d8ae94050854
Jluiz.007 <jluiz.placido@gmail.com>
2022-02-01 21:16:06 -0300

    First Commit IntelliJ

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+        /*int a = 6;
+        int b = 5;
+        System.out.println("Codando com Java " + (a+b));*/
+    }
+
+}
